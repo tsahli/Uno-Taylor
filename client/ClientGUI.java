739cef95a6867ccbc61f80e937554e42ac532551
Taylor Sahli <taylorsahl@mail.weber.edu>
2019-08-15 17:35:17 -0600

    Trying to get a GUI with "Draw Card" and "Play Card" working.

--- client/ClientGUI.java
+++ client/ClientGUI.java
@@ -8,6 +8,8 @@ import state.ClientState;
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -15,14 +17,54 @@ import java.net.Socket;
 
 public class ClientGUI extends JFrame {
     private static JTextArea textArea = new JTextArea(32, 64);
+    private static JButton drawButton = new JButton("Draw Card");
+    private static JButton playCardButton = new JButton("Play a Card");
+    private static String hostname;
+    private static int port = 9886;
+    private static Socket socket;
+    private static boolean validInput = false;
+    private static String cardNumber;
 
-    public ClientGUI() {
+    static {
+        try {
+            socket = new Socket(hostname, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ObjectOutputStream outToServer;
+
+    static {
+        try {
+            outToServer = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ObjectInputStream inFromServer;
+
+    static {
+        try {
+            inFromServer = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public ClientGUI() throws IOException {
         textArea.setEditable(false);
         textArea.setLineWrap(true);
         textArea.setMargin(new Insets(2, 2, 2, 2));
+        drawButton.addActionListener(new drawButtonListener());
+        playCardButton.addActionListener(new cardListener());
         Container container = getContentPane();
-        container.add(textArea, BorderLayout.CENTER);
-        container.add(new JScrollPane(textArea), BorderLayout.CENTER);
+        container.add(textArea, BorderLayout.NORTH);
+        container.add(drawButton, BorderLayout.SOUTH);
+        container.add(playCardButton, BorderLayout.CENTER);
+        container.add(new JScrollPane(textArea), BorderLayout.NORTH);
         DefaultCaret caret = (DefaultCaret) textArea.getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         setTitle("UNO");
@@ -35,24 +77,20 @@ public class ClientGUI extends JFrame {
         textArea.append(text + "\n");
     }
 
-
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ClientState gameState;
         Color[] allColors = new Color[]{Color.BLUE, Color.GREEN, Color.RED, Color.YELLOW};
         String hostname = JOptionPane.showInputDialog("Enter IP of game server: ");
         String username = JOptionPane.showInputDialog("Enter your username: ");
-        int port = 9886;
 
         ClientGUI frame;
         frame = new ClientGUI();
         boolean clientRunning = true;
         while (clientRunning) {
             addText("Waiting for all connections...");
-            Socket socket = new Socket(hostname, port);
 
             gameState = new ClientState();
-            ObjectOutputStream outToServer = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream inFromServer = new ObjectInputStream(socket.getInputStream());
+
 
             outToServer.writeObject(username);
             GameEvent event;
@@ -61,20 +99,20 @@ public class ClientGUI extends JFrame {
             while (gameActive) {
                 event = (GameEvent) inFromServer.readObject();
                 if (event.makeString().equals("YourTurn")) {
-                    boolean validInput = false;
+                    //validInput = false;
                     addText("\nIt's your turn!");
                     addText("Top card is: " + gameState.topCard.makeString());
                     addText("Your hand:");
                     addText(gameState.hand.printHand());
 
                     do {
-                        String response = JOptionPane.showInputDialog("Enter number to play card, 'p' to draw: ");
-                        if (response.toLowerCase().equals("p")) {
-                            outToServer.writeObject(new DrawCard());
-                            validInput = true;
-                        } else {
+                        //String response = JOptionPane.showInputDialog("Enter number to play card, 'p' to draw: ");
+                        if (drawButton.getModel().isPressed()) {
+                            addText("You drew a card!");
+                        } else if (cardNumber != "") {
                             try {
-                                int number = Integer.valueOf(response).intValue();
+                                //String cardNumber = JOptionPane.showInputDialog("Enter the number of the card you want to play: ");
+                                int number = Integer.valueOf(cardNumber).intValue();
                                 Card card = gameState.hand.cards.get(number - 1);
                                 if (!card.canPlaceOn(gameState.topCard)) {
                                     addText("You can't play that card!");
@@ -105,8 +143,10 @@ public class ClientGUI extends JFrame {
                                     }
                                     outToServer.writeObject(new PlaceCard(card));
                                     validInput = true;
+                                    cardNumber = "";
                                 }
                             } catch (NumberFormatException e) {
+                                cardNumber = "";
                                 validInput = false;
                             } catch (IndexOutOfBoundsException e) {
                                 validInput = false;
@@ -179,6 +219,25 @@ public class ClientGUI extends JFrame {
                     validPlayAgainInput = false;
                 }
             } while (!validPlayAgainInput);
+        }
+    }
+
+    private class drawButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                outToServer.writeObject(new DrawCard());
+                validInput = true;
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private class cardListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cardNumber = JOptionPane.showInputDialog("Enter number of card you want to play: ");
         }
     }
 }
